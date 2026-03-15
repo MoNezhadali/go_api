adca59f4b7707bd36ffd67f3a0e3b916f8ef79dd
MoNezhadali <monezhadali@gmail.com>
2026-03-15 22:42:33 +0100

    Add api/api.go

---
+++ api/api.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Coin Balance Parms
+type CoinBalanceParams struct{
+	// The username for the account
+	Username string
+}
+
+// Coin Balance Respone
+type CoinBalanceResponse struct{
+	// Success code, usually 200
+	Code int
+	// Account Balance
+	Balance int64
+}
+
+type Error struct{
+	// Error code
+	Code int
+	// Error message
+	Message string
+}
+
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code: code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+var (
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An unexpected Error Occurred.", http.StatusInternalServerError)
+	}
+)
