1d79746b749b83a8451e58b7f9e15a7e4aedc35f
mone <mohammad.nezhadali@stingray.no>
2026-03-16 16:19:57 +0100

    Add a comment for StripSlashes

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -10,6 +10,7 @@ func Handler (r *chi.Mux) {
 	// Middleware is a function a function that is called before the primary function handles the endpoint.
 	// you can add middleware to a rout by .Use function
 	// Global middleware (It is applied all the time)
+	// All StripSlashes middleware will remove the trailing slash from the URL path, so /account/ will be treated as /account
 	r.Use(chimiddle.StripSlashes)
 
 	r.Route("/account", func(router chi.Router) {
